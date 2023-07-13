168633d475e02200164a970fae7c00ddf14f9ef9
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:57:10 +0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,4 +1,4 @@
-getgenv().Games = {
+getgenv().TQ = {
     -------------------------------------Treasure-Quest-------------------------------------------------------
     [2960777560] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
     [2978518061] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
@@ -7,6 +7,13 @@ getgenv().Games = {
     [11650685514] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-2",
     [8793520828] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
     [10127152911] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
-    [6198225400] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1",
+    ----------------------------------------------------------------------------------------------------------
+}
+getgenv().LRW = {
+    --------------------------------------Legends Re:Written--------------------------------------------------
+    [7816227995] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1",
+    [11650685514] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-2",
+    [8793520828] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
+    [10127152911] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
     ----------------------------------------------------------------------------------------------------------
 }
