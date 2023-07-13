68214f860d385e14f12c728c16ba4282ec98afea
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 01:36:10 +0700

    Update 1.TQ.lua

--- 1.TQ.lua
+++ 1.TQ.lua
@@ -1,6 +1,6 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
-local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/2.LRW1.lua"))()
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/games.lua"))()
 
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
 queue_on_teleport[[repeat wait() until game:IsLoaded() 
