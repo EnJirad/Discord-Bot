8ec1fecf049d2cf79fe9e4b5438e7a1f4a0a609a
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:24:15 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -23,6 +23,10 @@ local Pcall = pcall(function()
     end
 end)
 
+if isfile("V.G Hub//" .. Name) and readfile("V.G Hub//" .. Name) then
+    Settings = HttpService:JSONDecode(readfile("V.G Hub//" .. Name))
+end
+
 local OldNameCall = nil
 OldNameCall = hookmetamethod(game, "__namecall", function(...)
     local Args = {...}
