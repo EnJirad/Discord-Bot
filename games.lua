ce58f4148364a96a47b858057f3e334604719f22
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-13 11:41:43 +0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,5 +1,6 @@
 local games = {
     [2960777560] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
+    [2978518061] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
     [6198225400] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1",
 }
 
