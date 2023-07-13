bfd57c21fa6e126e563fb559c8834f78833e0a3d
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-13 15:36:36 +0700

    Update MainXhub.lua

--- MainXhub.lua
+++ MainXhub.lua
@@ -1,4 +1,4 @@
-repeat task.wait(2) until game:IsLoaded()
+repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
 local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/games.lua"))()
 
@@ -9,7 +9,7 @@ for i, v in pairs(Games) do
 end
 
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
-queue_on_teleport[[repeat wait(2) until game:IsLoaded() 
+queue_on_teleport[[repeat wait() until game:IsLoaded() 
 print("ServerHoped or rejoined") 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/MainXhub.lua'))()
 ]]
