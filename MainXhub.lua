535ce75ebd4c767b297d1e8b6b5dfb66d1f42138
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:02:51 +0700

    Update MainXhub.lua

--- MainXhub.lua
+++ MainXhub.lua
@@ -2,12 +2,6 @@ repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
 local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest"))()
 
-for i, v in pairs(Games) do
-    if i == game.PlaceId then
-        loadstring(game:HttpGet(v))()
-    end
-end
-
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
 queue_on_teleport[[repeat wait() until game:IsLoaded() 
 print("ServerHoped or rejoined") 
