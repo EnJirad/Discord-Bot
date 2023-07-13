7e189fe9b803be48436a91c8962cd258ea0ff17d
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:47:29 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -1,17 +1,3 @@
 repeat wait() until game:IsLoaded()
 local G = getgenv and getgenv() or _G or shared
 G.Get = setmetatable({}, {__index = function(A, B) return game:GetService(B) end})
-
-G.Player = Players.LocalPlayer
-G.wait = task.wait
-G.spawn = task.spawn
-G.Heartbeat = RunService.Heartbeat
-G.Stepped = RunService.Stepped
-G.RenderStepped = RunService.RenderStepped
-G.Idled = Player.Idled
-
-local Name = game.PlaceId .. ".json"
-local Des = {}
-if makefolder and not isfile("V.G Hub") then
-    makefolder("V.G Hub")
-end
