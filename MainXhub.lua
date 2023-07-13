61daea3487a71c7c89857df371aa4cc3a13a206b
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-13 14:34:59 +0700

    Update MainXhub.lua

--- MainXhub.lua
+++ MainXhub.lua
@@ -1,16 +1,21 @@
 repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
-local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/games.lua"))()
+
+local A = "https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/games.lua"
+
+local function RunScript(scriptUrl)
+    local script = game:HttpGet(scriptUrl)
+    loadstring(script)()
+end
+
+local games = loadstring(game:HttpGet(gamesUrl))()
+local scriptUrl = games[currentGameId]
+
+if scriptUrl then
+    RunScript(A)
+else
+    print("No")
+end
 
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
 queue_on_teleport[[repeat wait() until game:IsLoaded() print("ServerHoped or rejoined") loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/MainXhub.lua'))()]]
-
-for i, v in pairs(Games) do
-    if i == game.PlaceId then
-        loadstring(game:HttpGet(v))()
-    end
-end
-
-for i, v in pairs(Unknown) do
-    loadstring(game:HttpGet(v))()
-end
