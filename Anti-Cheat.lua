53f42e8a0de877c0746bcc9034a0a5573a9de32a
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:46:27 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -2,10 +2,6 @@ repeat wait() until game:IsLoaded()
 local G = getgenv and getgenv() or _G or shared
 G.Get = setmetatable({}, {__index = function(A, B) return game:GetService(B) end})
 
-for i,v in pairs(game.GetChildren(game)) do
-    G[v.ClassName] = v
-end
-
 G.Player = Players.LocalPlayer
 G.wait = task.wait
 G.spawn = task.spawn
@@ -13,27 +9,12 @@ G.Heartbeat = RunService.Heartbeat
 G.Stepped = RunService.Stepped
 G.RenderStepped = RunService.RenderStepped
 G.Error = ScriptContext.Error
-G.MessageOut = LogService.MessageOut
-G.Kick = Player.Kick
 G.Idled = Player.Idled
 
 local Name = game.PlaceId .. ".json"
 local Des = {}
 if makefolder and not isfile("V.G Hub") then
     makefolder("V.G Hub")
-end
-
-G.Settings = {}
-local Pcall = pcall(function()
-    if isfile("V.G Hub//" .. Name) then
-        readfile("V.G Hub//" .. Name)
-    else
-        writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Des))
-    end
-end)
-
-if isfile("V.G Hub//" .. Name) and readfile("V.G Hub//" .. Name) then
-    Settings = HttpService:JSONDecode(readfile("V.G Hub//" .. Name))
 end
 
 G.Save = function()
