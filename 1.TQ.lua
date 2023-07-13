2004e3fb8a86cbd9bc95dfe7e177151cfb254dfa
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 01:35:43 +0700

    Update 1.TQ.lua

--- 1.TQ.lua
+++ 1.TQ.lua
@@ -1,18 +1,9 @@
 repeat wait() until game:IsLoaded()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/2.LRW1.lua"))()
 
-local TQ = getgenv().TQ
-local queue_on_teleport = queue_on_teleport or (syn and syn.queue_on_teleport)
-
-if TQ then
-    local placeId = game.PlaceId
-    local scriptUrl = TQ[placeId]
-    if scriptUrl then
-        loadstring(game:HttpGet(scriptUrl))()
-    end
-end
-
-queue_on_teleport(function()
-    repeat wait() until game:IsLoaded()
-    print("Server Hopped or rejoined")
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/MainXhub.lua'))()
-end)
+local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
+queue_on_teleport[[repeat wait() until game:IsLoaded() 
+print("ServerHoped or rejoined") 
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/MainXhub.lua'))()
+]]
