58008d2c160ea14552f6d8b2cae03e65eb617614
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:21:45 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -27,27 +27,6 @@ if isfile("V.G Hub//" .. Name) and readfile("V.G Hub//" .. Name) then
     Settings = HttpService:JSONDecode(readfile("V.G Hub//" .. Name))
 end
 
-
-local Nos = {
-    "PreloadAsync",
-    "xpcall",
-    "gcinfo",
-    "collectgarbage",
-    "FindService",
-}
-
-local Yes = {
-    "Kick",
-    "kick",
-}
-
-local Disables = {
-    Error,
-    MessageOut,
-    Idled
-}
-
-
 local OldNameCall = nil
 OldNameCall = hookmetamethod(game, "__namecall", function(...)
     local Args = {...}
@@ -98,6 +77,3 @@ G.Save = function()
         writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Settings))
     end)
 end
-
-ScriptContext:SetTimeout(0)
-local getconstants=debug.getconstants or getconstants;local getidentity=get_thread_context or getthreadcontext or getidentity or syn.get_thread_identity;local setidentity=set_thread_context or setthreadcontext or setidentity or syn.set_thread_identity;local hookfunc=hookfunction or hookfunc or detour_function;for a,b in next,getgc()do if type(b)=="function"and islclosure(b)then local c=getconstants(b)if table.find(c,"Detected")and table.find(c,"crash")then hookfunc(b,function()return task.wait(math.huge)end)end end end
