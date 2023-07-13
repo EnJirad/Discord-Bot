eb750a55168c1174ebd3b2d5385b643e497c19f7
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-13 10:59:49 +0700

    Update and rename Hook.lua to IDgame.lua

---
+++ IDgame.lua
@@ -0,0 +1,9 @@
+idgame().Games = {
+    [2960777560] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Games/Adopt-Me",
+    [6198225400] = "https://raw.githubusercontent.com/1201for/V.G-Hub/main/Games/Arsenal",
+}
+
+idgame().Unknown = {
+    "https://raw.githubusercontent.com/1201for/V.G-Hub-Part-3/main/War",
+    "https://raw.githubusercontent.com/1201for/V.G-Hub-Part-3/main/DBFR-Worlds",
+}
