fc50fc5e68615c78bf12ed3e9439564938fba1f5
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:23:48 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -23,6 +23,22 @@ local Pcall = pcall(function()
     end
 end)
 
+local OldNameCall = nil
+OldNameCall = hookmetamethod(game, "__namecall", function(...)
+    local Args = {...}
+    local A, B, C = ...
+    if table.find(Yes, getnamecallmethod()) and A == Player then
+        return
+    end
+    if table.find(Nos, getnamecallmethod()) then
+        return
+    end
+    if type(A) ~= "Instance" then
+        return OldNameCall(...)
+    end
+    return OldNameCall(...)
+end)
+
 
 ScriptContext:SetTimeout(0)
 local getconstants=debug.getconstants or getconstants;local getidentity=get_thread_context or getthreadcontext or getidentity or syn.get_thread_identity;local setidentity=set_thread_context or setthreadcontext or setidentity or syn.set_thread_identity;local hookfunc=hookfunction or hookfunc or detour_function;for a,b in next,getgc()do if type(b)=="function"and islclosure(b)then local c=getconstants(b)if table.find(c,"Detected")and table.find(c,"crash")then hookfunc(b,function()return task.wait(math.huge)end)end end end
