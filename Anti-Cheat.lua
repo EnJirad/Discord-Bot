ca2d0066cafcbe432dd1f5779a0eeccefed69f39
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-13 13:38:44 +0700

    Create Anti-Cheat.lua

---
+++ Anti-Cheat.lua
@@ -0,0 +1,9 @@
+repeat wait() until game:IsLoaded()
+local G = getgenv and getgenv() or _G or shared
+G.Get = setmetatable({}, {__index = function(A, B) return game:GetService(B) end})
+
+for i,v in pairs(game.GetChildren(game)) do
+    G[v.ClassName] = v
+end
+
+G.Player = Players.LocalPlayer
