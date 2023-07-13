5c739f200681fce57ddf3a4a5feb601cc8c4aa85
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-13 11:23:03 +0700

    Update Games-List.lua

--- Games-List.lua
+++ Games-List.lua
@@ -1,9 +1,9 @@
-idgame().Games = {
+local games = {
     [2960777560] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
     [6198225400] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1",
 }
 
-idgame().Unknown = {
+local unknownScripts = {
     "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
     "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1",
 }
