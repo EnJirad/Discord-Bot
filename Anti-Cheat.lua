2a0093953618f8974d1bbbf2b8ac489667c83998
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 01:36:59 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -1,38 +1,49 @@
 repeat wait() until game:IsLoaded()
-
 local G = getgenv and getgenv() or _G or shared
 G.Get = setmetatable({}, {__index = function(A, B) return game:GetService(B) end})
 
-for _, v in pairs(game:GetChildren()) do
+for i,v in pairs(game.GetChildren(game)) do
     G[v.ClassName] = v
 end
 
-G.Player = game.Players.LocalPlayer
+G.Player = Players.LocalPlayer
 G.wait = task.wait
 G.spawn = task.spawn
-G.Heartbeat = game:GetService("RunService").Heartbeat
-G.Stepped = game:GetService("RunService").Stepped
-G.RenderStepped = game:GetService("RunService").RenderStepped
-G.Error = script.ScriptContext.Error
-G.MessageOut = game:GetService("LogService").MessageOut
-G.Kick = game.Players.LocalPlayer.Kick
-G.Idled = game.Players.LocalPlayer.Idled
+G.Heartbeat = RunService.Heartbeat
+G.Stepped = RunService.Stepped
+G.RenderStepped = RunService.RenderStepped
+G.Error = ScriptContext.Error
+G.MessageOut = LogService.MessageOut
+G.Kick = Player.Kick
+G.Idled = Player.Idled
 
 local Name = game.PlaceId .. ".json"
-local Settings = {}
-
-if not isfile("V.G Hub") then
+local Des = {}
+if makefolder and not isfile("V.G Hub") then
     makefolder("V.G Hub")
 end
 
-local Pcall, HttpService = pcall, game:GetService("HttpService")
-Pcall(function()
-    if isfile("V.G Hub/" .. Name) then
-        Settings = HttpService:JSONDecode(readfile("V.G Hub/" .. Name))
+G.Settings = {}
+local Pcall = pcall(function()
+    if isfile("V.G Hub//" .. Name) then
+        readfile("V.G Hub//" .. Name)
     else
-        writefile("V.G Hub/" .. Name, HttpService:JSONEncode(Settings))
+        writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Des))
     end
 end)
+
+if isfile("V.G Hub//" .. Name) and readfile("V.G Hub//" .. Name) then
+    Settings = HttpService:JSONDecode(readfile("V.G Hub//" .. Name))
+end
+
+
+local Nos = {
+    "PreloadAsync",
+    "xpcall",
+    "gcinfo",
+    "collectgarbage",
+    "FindService",
+}
 
 local Yes = {
     "Kick",
@@ -40,16 +51,20 @@ local Yes = {
 }
 
 local Disables = {
-    G.Error,
-    G.MessageOut,
-    G.Idled
+    Error,
+    MessageOut,
+    Idled
 }
+
 
 local OldNameCall = nil
 OldNameCall = hookmetamethod(game, "__namecall", function(...)
     local Args = {...}
-    local A = ...
-    if table.find(Yes, getnamecallmethod()) and A == G.Player then
+    local A, B, C = ...
+    if table.find(Yes, getnamecallmethod()) and A == Player then
+        return
+    end
+    if table.find(Nos, getnamecallmethod()) then
         return
     end
     if type(A) ~= "Instance" then
@@ -58,17 +73,17 @@ OldNameCall = hookmetamethod(game, "__namecall", function(...)
     return OldNameCall(...)
 end)
 
-if setfflag then
-    setfflag("HumanoidParallelRemoveNoPhysics", false)
-    setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", false)
-end
 
+if setfflag then
+    setfflag("HumanoidParallelRemoveNoPhysics", "False")
+    setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
+end
 if setfpscap then
     setfpscap(100)
 end
 
 G.GetFunction = function(A)
-    for _, v in ipairs(getgc()) do
+    for i,v in next, getgc() do
         if type(v) == "function" and getinfo(v).name == A and islclosure(v) then
             return v 
         end
@@ -83,14 +98,14 @@ G.Teleport = function(A, B, Toggle)
 end
 
 G.DisableConnection = function(A)
-    for _, v in ipairs(getconnections(A)) do 
+    for i,v in next, getconnections(A) do 
         v:Disable()
     end
     return A
 end
 
 G.FireConnection = function(A)
-    for _, v in ipairs(getconnections(A)) do
+    for i,v in next, getconnections(A) do
         v:Fire()
     end
 end
@@ -99,7 +114,7 @@ G.Tween = function(A, B, C)
     if A and B then
         local Time = (B.Position - A.Position).Magnitude / C 
         local Info = TweenInfo.new(Time, Enum.EasingStyle.Linear)
-        local Tween = game:GetService("TweenService"):Create(A, Info, {CFrame = CFrame.new(B.Position)})
+        local Tween = TweenService:Create(A, Info, {CFrame = CFrame.new(B.Position)})
         Tween:Play()
         if Tween.Completed then
             Tween.Completed:Wait()
@@ -110,7 +125,7 @@ end
 
 G.Save = function()
     pcall(function()
-        writefile("V.G Hub/" .. Name, HttpService:JSONEncode(Settings))
+        writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Settings))
     end)
 end
 
@@ -118,11 +133,10 @@ G.ServerHop = function()
     spawn(function()
         while wait() do
             pcall(function()
-                local Response = game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100')
-                local Data = HttpService:JSONDecode(Response)
-                for _, v in ipairs(Data.data) do
+                local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+                for i,v in next, Gay.data do
                     if v.playing < v.maxPlayers then
-                        game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, v.id, G.Player)
+                        TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
                         break
                     end
                 end
@@ -133,49 +147,32 @@ G.ServerHop = function()
 end
 
 G.Rejoin = function()
-    return game:GetService("TeleportService"):Teleport(game.PlaceId, G.Player)
+    return TeleportService:Teleport(game.PlaceId, Player)
 end
 
 G.NoClip = function(A)
-    return A:ChangeState(Enum.HumanoidStateType.Jumping)
+    return A:ChangeState(11)
 end
-
 G.NoClip2 = function(A)
-    for _, v in ipairs(A:GetChildren()) do
+    for i,v in next, A:GetChildren() do
         if v:IsA("BasePart") then
             v.CanCollide = false 
         end
     end
 end
-
 G.SendNotification = function(Title, Text, Icon, Duration)
-    return game:GetService("StarterGui"):SetCore("SendNotification", {Title = Title, Text = Text, Icon = Icon, Duration = Duration})
+    return StarterGui:SetCore("SendNotification", {Title = Title, Text = Text, Icon = Icon, Duration = Duration})
 end
 
 G.Mag = function(A, B)
     return (A.Position - B.Position).Magnitude
 end
 
-for _, v in ipairs(Disables) do 
-    for _, connection in ipairs(getconnections(v)) do
-        connection:Disable()
+for i,v in next, Disables do 
+    for i,v in next, getconnections(v) do
+        v:Disable()
     end
 end
 
-script.ScriptContext:SetTimeout(0)
-
-local getconstants = debug.getconstants or getconstants
-local getidentity = get_thread_context or getthreadcontext or getidentity or syn.get_thread_identity
-local setidentity = set_thread_context or setthreadcontext or setidentity or syn.set_thread_identity
-local hookfunc = hookfunction or hookfunc or detour_function
-
-for _, func in ipairs(getgc()) do
-    if type(func) == "function" and islclosure(func) then
-        local constants = getconstants(func)
-        if table.find(constants, "Detected") and table.find(constants, "crash") then
-            hookfunc(func, function()
-                return task.wait(math.huge)
-            end)
-        end
-    end
-end
+ScriptContext:SetTimeout(0)
+local getconstants=debug.getconstants or getconstants;local getidentity=get_thread_context or getthreadcontext or getidentity or syn.get_thread_identity;local setidentity=set_thread_context or setthreadcontext or setidentity or syn.set_thread_identity;local hookfunc=hookfunction or hookfunc or detour_function;for a,b in next,getgc()do if type(b)=="function"and islclosure(b)then local c=getconstants(b)if table.find(c,"Detected")and table.find(c,"crash")then hookfunc(b,function()return task.wait(math.huge)end)end end end
