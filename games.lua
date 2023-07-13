8c7ad341f3a4635bf08a0ef2604e1cda82c260fd
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:36:46 +0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -9,15 +9,3 @@ getgenv().Games = {
     [10127152911] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
     ----------------------------------------------------------------------------------------------------------
 }
-
-getgenv().Unknown = {
-    -------------------------------------Treasure-Quest-------------------------------------------------------
-    "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
-    "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
-    --------------------------------------Legends Re:Written--------------------------------------------------
-    "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1",
-    "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-2",
-    "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
-    "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
-    ----------------------------------------------------------------------------------------------------------
-}
