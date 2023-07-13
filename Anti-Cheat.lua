df2994e7cf18d8765b4da82204be8ff09a151078
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 00:48:47 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -1,3 +1,109 @@
 repeat wait() until game:IsLoaded()
 local G = getgenv and getgenv() or _G or shared
 G.Get = setmetatable({}, {__index = function(A, B) return game:GetService(B) end})
+
+for i,v in pairs(game.GetChildren(game)) do
+    G[v.ClassName] = v
+end
+
+G.Player = Players.LocalPlayer
+G.wait = task.wait
+G.spawn = task.spawn
+G.Heartbeat = RunService.Heartbeat
+G.Stepped = RunService.Stepped
+G.RenderStepped = RunService.RenderStepped
+G.Error = ScriptContext.Error
+G.MessageOut = LogService.MessageOut
+G.Kick = Player.Kick
+G.Idled = Player.Idled
+
+local Name = game.PlaceId .. ".json"
+local Des = {}
+if makefolder and not isfile("V.G Hub") then
+    makefolder("V.G Hub")
+end
+
+G.Settings = {}
+local Pcall = pcall(function()
+    if isfile("V.G Hub//" .. Name) then
+        readfile("V.G Hub//" .. Name)
+    else
+        writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Des))
+    end
+end)
+
+if isfile("V.G Hub//" .. Name) and readfile("V.G Hub//" .. Name) then
+    Settings = HttpService:JSONDecode(readfile("V.G Hub//" .. Name))
+end
+
+
+local Nos = {
+    "PreloadAsync",
+    "xpcall",
+    "gcinfo",
+    "collectgarbage",
+    "FindService",
+}
+
+local Yes = {
+    "Kick",
+    "kick",
+}
+
+local OldNameCall = nil
+OldNameCall = hookmetamethod(game, "__namecall", function(...)
+    local Args = {...}
+    local A, B, C = ...
+    if table.find(Yes, getnamecallmethod()) and A == Player then
+        return
+    end
+    if table.find(Nos, getnamecallmethod()) then
+        return
+    end
+    if type(A) ~= "Instance" then
+        return OldNameCall(...)
+    end
+    return OldNameCall(...)
+end)
+
+
+if setfflag then
+    setfflag("HumanoidParallelRemoveNoPhysics", "False")
+    setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
+end
+if setfpscap then
+    setfpscap(100)
+end
+
+G.GetFunction = function(A)
+    for i,v in next, getgc() do
+        if type(v) == "function" and getinfo(v).name == A and islclosure(v) then
+            return v 
+        end
+    end
+end
+
+G.DisableConnection = function(A)
+    for i,v in next, getconnections(A) do 
+        v:Disable()
+    end
+    return A
+end
+
+G.FireConnection = function(A)
+    for i,v in next, getconnections(A) do
+        v:Fire()
+    end
+end
+
+G.Save = function()
+    pcall(function()
+        writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Settings))
+    end)
+end
+
+G.Mag = function(A, B)
+    return (A.Position - B.Position).Magnitude
+end
+
+ScriptContext:SetTimeout(0)
