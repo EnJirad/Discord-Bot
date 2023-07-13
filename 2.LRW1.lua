a848125fa104d9a911813b40d1cc6c7293a63a1a
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 01:11:39 +0700

    Update 2.LRW1.lua

--- 2.LRW1.lua
+++ 2.LRW1.lua
@@ -1,12 +1,6 @@
 repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
-local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/games.lua"))()
-
-for i, v in pairs(LRW1) do
-    if i == game.PlaceId then
-        loadstring(game:HttpGet(v))()
-    end
-end
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/2.LRW1.lua"))()
 
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
 queue_on_teleport[[repeat wait() until game:IsLoaded() 
