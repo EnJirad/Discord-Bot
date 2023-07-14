e1153b6087285eeec9978b5bf58620726d07e50c
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 12:20:29 +0700

    Update 1.TQ.lua

--- 1.TQ.lua
+++ 1.TQ.lua
@@ -1,19 +1,15 @@
 repeat wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
+local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/games.lua"))()
 
-local TQScripts = {
-    [2960777560] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
-    [2978518061] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest"
-}
-
-local TQScriptURL = TQScripts[game.PlaceId]
-
-if TQScriptURL then
-    loadstring(game:HttpGet(TQScriptURL))()
+for i, v in pairs(TQ) do
+    if i == game.PlaceId then
+        loadstring(game:HttpGet(v))()
+    end
 end
 
 local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
 queue_on_teleport[[repeat wait() until game:IsLoaded() 
-print("Server Hopped or rejoined") 
+print("ServerHoped or rejoined") 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/MainXhub.lua'))()
 ]]
