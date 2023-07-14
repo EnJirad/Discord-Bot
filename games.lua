cd2e3e3ff42d2484ea0c173315bea561c20f0597
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 12:30:21 +0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,4 +1,4 @@
-getgenv().Game = {
+getgenv().Games = {
     -------------------------------------Treasure-Quest-------------------------------------------------------
     [2960777560] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
     [2978518061] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest"
