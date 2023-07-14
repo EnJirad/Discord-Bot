8410c0ef016770d8d0a89cdf9ef93e6284ee9999
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 12:32:30 +0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -6,6 +6,6 @@ getgenv().Games = {
     [7816227995] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1",
     [11650685514] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-2",
     [8793520828] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
-    [10127152911] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid"
+    [10127152911] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
     -----------------------------------------------------------------------------------------------------------
 }
