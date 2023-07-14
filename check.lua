7c3f1fd27dd019526cb809a50698dc1e240f389a
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 12:39:29 +0700

    Update check.lua

--- check.lua
+++ check.lua
@@ -24,7 +24,7 @@ local function CheckID(ID)
 end
 
 local function HUB()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/MainXhub.lua'))()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/1.TQ.lua'))()
 end
 
 if CheckKey(Key) and CheckID(ID) then
