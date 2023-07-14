d60f12ff615bbcb70d8b6c2777dc54548199572e
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 13:12:38 +0700

    Update 3.LRW2.lua

--- LRW2/3.LRW2.lua
+++ LRW2/3.LRW2.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
 local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/games.lua"))()
 
-for i, v in pairs(TQ) do
+for i, v in pairs(LRW2) do
     if i == game.PlaceId then
         loadstring(game:HttpGet(v))()
     end
