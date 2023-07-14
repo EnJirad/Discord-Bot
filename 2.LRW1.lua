985e0c546658ba1ea031d345f69dfc5a1ce862e2
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-14 12:58:08 +0700

    Delete 2.LRW1.lua

--- 2.LRW1.lua
+++
@@ -1,9 +0,0 @@
-repeat task.wait() until game:IsLoaded()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/Anti-Cheat.lua"))()
-local A = loadstring(game:HttpGet("https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/2.LRW1.lua"))()
-
-local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport 
-queue_on_teleport[[repeat wait() until game:IsLoaded() 
-print("ServerHoped or rejoined") 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/MainXhub.lua'))()
-]]
