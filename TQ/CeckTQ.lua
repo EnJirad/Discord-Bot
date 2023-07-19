8a9b637285a66ab5662cae5375294d9d8ecb17c5
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-19 21:30:30 +0700

    Update CeckTQ.lua

--- TQ/CeckTQ.lua
+++ TQ/CeckTQ.lua
@@ -1,29 +1,9 @@
-local HttpService = game:GetService("HttpService")
-local whitelistURL = "https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/TQ/UserTQ.json"
-local Key = _G.Key
-
-local function CheckKey(Key)
-    local whitelist = HttpService:JSONDecode(game:HttpGet(whitelistURL))
-    for _, user in pairs(whitelist.users) do
-        if Key == user.Key and string.len(Key) == 16 then
-            return true
-        end
+game:GetService("TeleportService").TeleportInitFailed:Connect(function(player, teleportResult, errorMessage)
+    -- ตรวจสอบว่าผู้เล่นที่ย้ายเซิร์ฟเวอร์เป็นผู้เล่นปัจจุบันหรือไม่
+    if player == game.Players.LocalPlayer then
+        -- เมื่อเกิดการล้มเหลวในการเชื่อมต่อเซิร์ฟเวอร์ใหม่
+        print("เกิดข้อผิดพลาดในการเชื่อมต่อเซิร์ฟเวอร์ใหม่: " .. errorMessage)
+        -- รันสคริปต์ตามที่คุณต้องการทำ
+        loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'))()
     end
-    return false
-end
-
-local function HUB()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'))()
-end
-
-game:GetService("TeleportService").TeleportInitFailed:Connect(function()
-    print("Server change detected. Running script...")
-    HUB()
 end)
-
-if CheckKey(Key) then
-    print("Yes")
-    HUB()
-else
-    print("No")
-end
