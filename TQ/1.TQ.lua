e0e732e10077fa51a0dc7f15dd5b958ac01ece24
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-19 21:14:55 +0700

    Update 1.TQ.lua

--- TQ/1.TQ.lua
+++ TQ/1.TQ.lua
@@ -8,9 +8,3 @@ gamemap().TQ = {
 if gamemap().TQ[game.PlaceId] then
     loadstring(game:HttpGet(gamemap().TQ[game.PlaceId]))()
 end
-
-local queue_on_teleport = queue_on_teleport or syn and syn.queue_on_teleport
-queue_on_teleport([[repeat wait() until game:IsLoaded() 
-print("Server Hopped or rejoined") 
-loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/TQ/1.TQ.lua'))()
-]])
