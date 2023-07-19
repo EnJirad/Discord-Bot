c1acc8f94e7ad4ec1ae607ca1e55c06cadf094e9
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-19 22:03:36 +0700

    Update 1.TQ.lua

--- TQ/1.TQ.lua
+++ TQ/1.TQ.lua
@@ -1,11 +1,1 @@
-repeat
-    wait()
-until game:IsLoaded()
-
-local A,B = pcall(function()
-    loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'))()
-end)
-
-if A == false then
-    print(B)
-end
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'))()
