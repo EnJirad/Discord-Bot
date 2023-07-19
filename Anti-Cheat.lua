b464edd784d965b48cc55a33b6648c4c57c05975
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-19 20:04:35 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -1,50 +1,21 @@
 repeat wait() until game:IsLoaded()
 
 local G = getgenv and getgenv() or _G or shared
-G.Get = setmetatable({}, {__index = function(A, B) return game:GetService(B) end})
 
 G.Player = Players.LocalPlayer
 G.wait = task.wait
 
 local Name = game.PlaceId .. ".json"
-local Des = {}
-if makefolder and not isfile("V.G Hub") then
-    makefolder("V.G Hub")
-end
-
 G.Settings = {}
-local Pcall = pcall(function()
-    if isfile("V.G Hub//" .. Name) then
-        readfile("V.G Hub//" .. Name)
-    else
-        writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Des))
-    end
-end)
 
-if isfile("V.G Hub//" .. Name) and readfile("V.G Hub//" .. Name) then
+if isfile("V.G Hub//" .. Name) then
     G.Settings = HttpService:JSONDecode(readfile("V.G Hub//" .. Name))
 end
 
-local Disables = {
-    G.Error,
-    G.MessageOut,
-    G.Idled
-}
-
 local OldNameCall = nil
 OldNameCall = hookmetamethod(game, "__namecall", function(...)
-    local Args = {...}
-    local A, B, C = ...
-    if type(A) ~= "Instance" then
-        return OldNameCall(...)
-    end
     return OldNameCall(...)
 end)
-
-if setfflag then
-    setfflag("HumanoidParallelRemoveNoPhysics", "False")
-    setfflag("HumanoidParallelRemoveNoPhysicsNoSimulate2", "False")
-end
 
 G.NoClip = function(A)
     return A:ChangeState(11)
@@ -80,5 +51,3 @@ G.ServerHop = function()
         end
     end)
 end
-
-ScriptContext:SetTimeout(0)
