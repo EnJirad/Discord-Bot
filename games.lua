165552d6456dfce2c73b04b3c719b0d19f07518d
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-19 21:06:37 +0700

    Update games.lua

--- games.lua
+++ games.lua
@@ -1,20 +1,20 @@
-map().TQ = {
+gamemap().TQ = {
     -------------------------------------Treasure-Quest-------------------------------------------------------
     [2960777560] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest",
     [2978518061] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest"
     ----------------------------------------------------------------------------------------------------------
 }
-map().LRW1 = {
+getgenv().LRW1 = {
     --------------------------------------Legends Re:Written 1------------------------------------------------
     [7816227995] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-1"
     ----------------------------------------------------------------------------------------------------------
 }
-map().LRW2 = {
+getgenv().LRW2 = {
     --------------------------------------Legends Re:Written 2-------------------------------------------------
     [11650685514] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Word-2"
     ----------------------------------------------------------------------------------------------------------
 }
-map().LRWRaid = {
+getgenv().LRWRaid = {
     --------------------------------------Legends Re:Written Raid----------------------------------------------
     [8793520828] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid",
     [10127152911] = "https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Legends-Re%3AWritten-Raid"
