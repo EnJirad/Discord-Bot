ddaedc52ed9a669a58ca5de034d36ddbef2cf8c3
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-19 19:53:48 +0700

    Update Anti-Cheat.lua

--- Anti-Cheat.lua
+++ Anti-Cheat.lua
@@ -90,9 +90,59 @@ G.GetFunction = function(A)
     end
 end
 
+G.Teleport = function(A, B, Toggle)
+    if Toggle and A and B then
+        A.CFrame = B
+    end
+    return A, B, Toggle
+end
+
+G.DisableConnection = function(A)
+    for i,v in next, getconnections(A) do 
+        v:Disable()
+    end
+    return A
+end
+
+G.FireConnection = function(A)
+    for i,v in next, getconnections(A) do
+        v:Fire()
+    end
+end
+
+G.Tween = function(A, B, C)
+    if A and B then
+        local Time = (B.Position - A.Position).Magnitude / C 
+        local Info = TweenInfo.new(Time, Enum.EasingStyle.Linear)
+        local Tween = TweenService:Create(A, Info, {CFrame = CFrame.new(B.Position)})
+        Tween:Play()
+        if Tween.Completed then
+            Tween.Completed:Wait()
+        end
+    end
+    return A, B, C
+end
+
 G.Save = function()
     pcall(function()
         writefile("V.G Hub//" .. Name, HttpService:JSONEncode(Settings))
+    end)
+end
+
+G.ServerHop = function()
+    spawn(function()
+        while wait() do
+            pcall(function()
+                local Gay = HttpService:JSONDecode(game:HttpGet('https://games.roblox.com/v1/games/' .. game.PlaceId .. '/servers/Public?sortOrder=Asc&limit=100'))
+                for i,v in next, Gay.data do
+                    if v.playing < v.maxPlayers then
+                        TeleportService:TeleportToPlaceInstance(game.PlaceId, v.id, Player)
+                        break
+                    end
+                end
+            end)
+            wait(4)
+        end
     end)
 end
 
@@ -117,3 +167,12 @@ end
 G.Mag = function(A, B)
     return (A.Position - B.Position).Magnitude
 end
+
+for i,v in next, Disables do 
+    for i,v in next, getconnections(v) do
+        v:Disable()
+    end
+end
+
+ScriptContext:SetTimeout(0)
+local getconstants=debug.getconstants or getconstants;local getidentity=get_thread_context or getthreadcontext or getidentity or syn.get_thread_identity;local setidentity=set_thread_context or setthreadcontext or setidentity or syn.set_thread_identity;local hookfunc=hookfunction or hookfunc or detour_function;for a,b in next,getgc()do if type(b)=="function"and islclosure(b)then local c=getconstants(b)if table.find(c,"Detected")and table.find(c,"crash")then hookfunc(b,function()return task.wait(math.huge)end)end end end
