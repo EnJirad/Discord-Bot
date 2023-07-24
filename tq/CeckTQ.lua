4c8b6d869e0cd951dd1cc847616464b95ec78f59
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-24 15:30:39 +0700

    Add files via upload

---
+++ tq/CeckTQ.lua
@@ -0,0 +1,24 @@
+local HttpService = game:GetService("HttpService")
+local whitelistURL = "https://raw.githubusercontent.com/EnJirad/Discord-Bot/main/TQ/UserTQ.json"
+local Key = _G.Key
+
+local function CheckKey(Key)
+    local whitelist = HttpService:JSONDecode(game:HttpGet(whitelistURL))
+    for _, user in pairs(whitelist.users) do
+        if Key == user.Key and string.len(Key) == 16 then
+            return true
+        end
+    end
+    return false
+end
+
+local function HUB()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'))()
+end
+
+if CheckKey(Key) then
+    print("Yes")
+    HUB()
+else
+    print("No")
+end
