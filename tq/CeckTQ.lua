ef30303f11d85d3e19f0d72f461905d0743ca4a1
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-25 14:03:46 +0700

    Update CeckTQ.lua

--- tq/CeckTQ.lua
+++ tq/CeckTQ.lua
@@ -3,31 +3,21 @@ local whitelistURL = "https://raw.githubusercontent.com/EnJirad/Discord-Bot/main
 local Key = _G.Key
 
 local function CheckKey(Key)
-    local whitelist = HttpService:JSONDecode(game:HttpGet(whitelistURL))
-    for _, user in pairs(whitelist.users) do
-        if Key == user.Key and string.len(Key) == 16 then
+    local result = game:HttpGet(whitelistURL)
+    local keys = result:split("\n")
+    for i = 1, #keys do
+        if string.match(keys[i], Key) and string.len(Key) == 16 then
             return true
         end
     end
     return false
 end
 
+
 local function HUB()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'))()
 end
 
--- ฟังก์ชันในการแปลง JSON เป็นตาราง (table)
-local function JSONToTable(jsonString)
-    return HttpService:JSONDecode(jsonString)
-end
-
--- โหลด JSON จาก URL
-local jsonStr = game:HttpGet(whitelistURL)
-
--- เรียกใช้ฟังก์ชันในการแปลง JSON เป็นตาราง
-local whitelistTable = JSONToTable(jsonStr)
-
--- เช็คค่า Key ในตาราง
 if CheckKey(Key) then
     print("Yes")
     HUB()
