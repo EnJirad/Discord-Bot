c3673e35b1ea94044e118b88bb895bb1bae7a25d
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-25 13:40:43 +0700

    Update CeckTQ.lua

--- tq/CeckTQ.lua
+++ tq/CeckTQ.lua
@@ -3,10 +3,15 @@ local whitelistURL = "https://raw.githubusercontent.com/EnJirad/Discord-Bot/main
 local Key = _G.Key
 
 local function CheckKey(Key)
-    local whitelist = HttpService:JSONDecode(game:HttpGet(whitelistURL))
-    for _, user in pairs(whitelist.users) do
-        if Key == user.Key and string.len(Key) == 16 then
-            return true
+    local success, whitelist = pcall(function()
+        return HttpService:JSONDecode(game:HttpGet(whitelistURL))
+    end)
+
+    if success and type(whitelist) == "table" and whitelist.users then
+        for _, user in pairs(whitelist.users) do
+            if Key == user.Key and string.len(Key) == 16 then
+                return true
+            end
         end
     end
     return false
