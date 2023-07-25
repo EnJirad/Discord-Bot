fd4b6de917f93647c54d49e3e265ccc96a7146fd
EnJirad <130825937+EnJirad@users.noreply.github.com>
2023-07-25 13:52:25 +0700

    Update CeckTQ.lua

--- tq/CeckTQ.lua
+++ tq/CeckTQ.lua
@@ -3,29 +3,22 @@ local whitelistURL = "https://raw.githubusercontent.com/EnJirad/Discord-Bot/main
 local Key = _G.Key
 
 local function CheckKey(Key)
-    local success, whitelist = pcall(function()
-        return HttpService:JSONDecode(game:HttpGet(whitelistURL))
-    end)
-
-    if success and type(whitelist) == "table" and whitelist.users then
-        for _, user in ipairs(whitelist.users) do
-            if Key == user.Key and string.len(Key) == 16 then
-                return true
-            end
+    local whitelist = HttpService:JSONDecode(game:HttpGet(whitelistURL))
+    for _, user in pairs(whitelist.users) do
+        if Key == user.Key and string.len(Key) == 16 then
+            return true
         end
     end
     return false
 end
 
 local function HUB()
-    local scriptURL = 'https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'
-    local scriptContent = game:HttpGet(scriptURL)
-    loadstring(scriptContent)()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/EnJirad/Script-Roblox/main/Treasure-Quest'))()
 end
 
 if CheckKey(Key) then
-    HUB()
     print("Yes")
+    HUB()
 else
     print("No")
 end
